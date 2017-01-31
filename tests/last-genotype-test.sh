8d74096dc9a86fe6f71a0a6e0bf5f963a5e51352
Martin C. Frith <mcfrith@gmail.com>
2017-01-31 12:57:22 +0900

    added tests for -f and -s options

--- tests/last-genotype-test.sh
+++ tests/last-genotype-test.sh
@@ -18,5 +18,7 @@ PATH=..:$PATH
     try last-genotype -m5 hs-rna.mat hs-rna.maf
     try last-genotype -p1 hs-rna.mat hs-rna.maf
     try last-genotype -p3 hs-rna.mat hs-rna.maf
+    try last-genotype -f1e6 -s50 hs-rna.mat hs-rna.maf
+    try last-genotype -f1000 hs-rna.mat hs-rna.maf
 } 2>&1 |
 diff -u $(basename $0 .sh).out -
